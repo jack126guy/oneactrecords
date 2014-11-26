1fcac166d30e95857dc754b815181baf158dabee
someguy126 <someanon126@gmail.com>
2014-11-25 22:51:16 -0800

    Convert indentation to tabs

--- releaselist.php
+++ releaselist.php
@@ -2,8 +2,8 @@
 include('db.php');
 $releasequery = $sql->query('SELECT releaseid, releasename, releasedate, releasecover FROM ' . $sql->get_table_prefix() . 'releases ORDER BY releasedate DESC');
 while($releaserow = $sql->fetch_assoc($releasequery)) {
- //Check hidden release
- if($sql->num_rows($sql->query('SELECT releaseid FROM ' . $sql->get_table_prefix() . 'hiddenrels WHERE releaseid = "' . $sql->real_escape_string($releaserow['releaseid']) . '"')) > 0) continue;
- echo '<div><a href="release.php?r=' . $releaserow['releaseid'] . '"><img class="thumb" alt="" src="' . $releaserow['releasecover'] . '"/><span class="releaselistitem">' . $releaserow['releasename'] . '</span></a></div>';
+	//Check hidden release
+	if($sql->num_rows($sql->query('SELECT releaseid FROM ' . $sql->get_table_prefix() . 'hiddenrels WHERE releaseid = "' . $sql->real_escape_string($releaserow['releaseid']) . '"')) > 0) continue;
+	echo '<div><a href="release.php?r=' . $releaserow['releaseid'] . '"><img class="thumb" alt="" src="' . $releaserow['releasecover'] . '"/><span class="releaselistitem">' . $releaserow['releasename'] . '</span></a></div>';
 }
 ?>
--- templates/links.php
+++ templates/links.php
@@ -1,27 +1,27 @@
 <?php
 if(!empty($tparams['releaseid'])) {
- echo '<ul>';
- $linksquery = $sql->query('SELECT * FROM ' . $sql->get_table_prefix() . 'links WHERE releaseid = "' . $tparams['releaseid'] . '" ORDER BY linkpos ASC');
- if($sql->error()) {
-  echo '<li>Error occured getting the links. Sorry :( Technical info: ' . $sql->error() . '</li>';
- } else {
-  if($sql->num_rows($linksquery) > 0) {
-   while($linksrow = $sql->fetch_assoc($linksquery)) {
-    echo '<li><span class="title"><a href="' . $linksrow['linkref'] . '">' . $linksrow['linkname'] . '</a></span><span class="desc">' . $linksrow['linkdesc'] . '</span><span class="techspec">' . $linksrow['linktech'] . '</span></li>';
-   }
-  }
- }
- //Get common links
- $clinksquery = $sql->query('SELECT * FROM ' . $sql->get_table_prefix() . 'linkforms INNER JOIN ' . $sql->get_table_prefix() . 'commonlinks ON ' . $sql->get_table_prefix() . 'linkforms.formname = ' . $sql->get_table_prefix() . 'commonlinks.formname WHERE releaseid = "' . $tparams['releaseid'] . '" ORDER BY formpos ASC');
- if($sql->error()) {
-  echo '<li>Error occured getting the links. Sorry :( Technical info: ' . $sql->error() . '</li>';
- } else {
-  if($sql->num_rows($clinksquery) > 0) {
-   while($clinksrow = $sql->fetch_assoc($clinksquery)) {
-    echo '<li><span class="title"><a href="' . $clinksrow['linkref'] . '">' . (empty($clinksrow['linkname']) ? $clinksrow['commname'] : $clinksrow['linkname']) . '</a></span><span class="desc">' . (empty($clinksrow['linkdesc']) ? $clinksrow['commdesc'] : $clinksrow['linkdesc']) . '</span><span class="techspec">' . (empty($clinksrow['linktech']) ? $clinksrow['commtech'] : $clinksrow['linktech']) . '</span></li>';
-   }
-  }
- }
- echo '</ul>';
+	echo '<ul>';
+	$linksquery = $sql->query('SELECT * FROM ' . $sql->get_table_prefix() . 'links WHERE releaseid = "' . $tparams['releaseid'] . '" ORDER BY linkpos ASC');
+	if($sql->error()) {
+		echo '<li>Error occured getting the links. Sorry :( Technical info: ' . $sql->error() . '</li>';
+	} else {
+		if($sql->num_rows($linksquery) > 0) {
+			while($linksrow = $sql->fetch_assoc($linksquery)) {
+				echo '<li><span class="title"><a href="' . $linksrow['linkref'] . '">' . $linksrow['linkname'] . '</a></span><span class="desc">' . $linksrow['linkdesc'] . '</span><span class="techspec">' . $linksrow['linktech'] . '</span></li>';
+			}
+		}
+	}
+	//Get common links
+	$clinksquery = $sql->query('SELECT * FROM ' . $sql->get_table_prefix() . 'linkforms INNER JOIN ' . $sql->get_table_prefix() . 'commonlinks ON ' . $sql->get_table_prefix() . 'linkforms.formname = ' . $sql->get_table_prefix() . 'commonlinks.formname WHERE releaseid = "' . $tparams['releaseid'] . '" ORDER BY formpos ASC');
+	if($sql->error()) {
+		echo '<li>Error occured getting the links. Sorry :( Technical info: ' . $sql->error() . '</li>';
+	} else {
+		if($sql->num_rows($clinksquery) > 0) {
+			while($clinksrow = $sql->fetch_assoc($clinksquery)) {
+				echo '<li><span class="title"><a href="' . $clinksrow['linkref'] . '">' . (empty($clinksrow['linkname']) ? $clinksrow['commname'] : $clinksrow['linkname']) . '</a></span><span class="desc">' . (empty($clinksrow['linkdesc']) ? $clinksrow['commdesc'] : $clinksrow['linkdesc']) . '</span><span class="techspec">' . (empty($clinksrow['linktech']) ? $clinksrow['commtech'] : $clinksrow['linktech']) . '</span></li>';
+			}
+		}
+	}
+	echo '</ul>';
 }
 ?>
