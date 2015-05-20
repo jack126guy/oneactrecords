f0b7925931cf5aff67bdc7e8de4b89c0e71249c1
Kyanos <someanon126@gmail.com>
2015-05-20 14:47:29 -0700

    Improve readability of hidden-release check

--- releaselist.php
+++ releaselist.php
@@ -4,7 +4,10 @@ $releasequery = $sql->query('SELECT releaseid, releasename, releasedate, release
 echo '<ul id="releaselist">';
 while($releaserow = $sql->fetch_assoc($releasequery)) {
 	//Check hidden release
-	if($sql->num_rows($sql->query('SELECT releaseid FROM ' . $sql->get_table_prefix() . 'hiddenrels WHERE releaseid = "' . $sql->real_escape_string($releaserow['releaseid']) . '"')) > 0) continue;
+	$hiddenrelquery = $sql->query('SELECT releaseid FROM ' . $sql->get_table_prefix() . 'hiddenrels WHERE releaseid = "' . $sql->real_escape_string($releaserow['releaseid']) . '"');
+	if(!$sql->error() && ($sql->num_rows($hiddenrelquery) > 0)) {
+		continue;
+	}
 	echo '<li><a href="release.php?r=' . $releaserow['releaseid'] . '"><img class="thumb" alt="" src="' . $releaserow['releasecover'] . '"/><span class="releaselistitem">' . $releaserow['releasename'] . '</span></a></li>';
 }
 echo '</ul>';
