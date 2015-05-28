b0e6f0f2d5c8727744cf06e55f31e8ad262e4d5a
Kyanos <someanon126@gmail.com>
2015-05-27 23:28:48 -0700

    Refactor link template and improve accessibility
    * Move code that was duplicated between specific and common links
      to a dedicated function
    * Expand code to multiple lines for readability
    * Simplify ternary checks for common links
      (implicit boolean casting instead of empty())
    * Add hidden text (for text-based browsers and screen readers)

--- templates/links.php
+++ templates/links.php
@@ -7,7 +7,7 @@ if(!empty($tparams['releaseid'])) {
 	} else {
 		if($sql->num_rows($linksquery) > 0) {
 			while($linksrow = $sql->fetch_assoc($linksquery)) {
-				echo '<li><span class="title"><a href="' . $linksrow['linkref'] . '">' . $linksrow['linkname'] . '</a></span> <span class="desc">' . $linksrow['linkdesc'] . '</span> <span class="techspec">' . $linksrow['linktech'] . '</span></li>';
+				echo oar_links_generate($linksrow['linkname'], $linksrow['linkref'], $linksrow['linkdesc'], $linksrow['linktech']);
 			}
 		}
 	}
@@ -18,10 +18,44 @@ if(!empty($tparams['releaseid'])) {
 	} else {
 		if($sql->num_rows($clinksquery) > 0) {
 			while($clinksrow = $sql->fetch_assoc($clinksquery)) {
-				echo '<li><span class="title"><a href="' . $clinksrow['linkref'] . '">' . (empty($clinksrow['linkname']) ? $clinksrow['commname'] : $clinksrow['linkname']) . '</a></span> <span class="desc">' . (empty($clinksrow['linkdesc']) ? $clinksrow['commdesc'] : $clinksrow['linkdesc']) . '</span> <span class="techspec">' . (empty($clinksrow['linktech']) ? $clinksrow['commtech'] : $clinksrow['linktech']) . '</span></li>';
+				echo oar_links_generate(
+					$clinksrow['linkname'] ? $clinksrow['linkname'] : $clinksrow['commname'],
+					$clinksrow['linkref'],
+					$clinksrow['linkdesc'] ? $clinksrow['linkdesc'] : $clinksrow['commdesc'],
+					$clinksrow['linktech'] ? $clinksrow['linktech'] : $clinksrow['commtech']
+				);
 			}
 		}
 	}
 	echo '</ul>';
+}
+
+/**
+ * Generate a link based on the given information.
+ * @param string $name Name of link
+ * @param string $ref Link target
+ * @param string $desc Description
+ * @param string $tech Technical info
+ * @return string HTML code for the link
+ */
+function oar_links_generate($name, $ref, $desc, $tech) {
+	$result = '';
+	$result .= '<li>';
+	//Name
+	$result .= '<span class="title"><a href="' . $ref . '">' . $name . '</a></span>';
+	//Description
+	if($desc) {
+		$result .= '<span class="hidden">: </span>';
+		$result .= '<span class="desc">' . $desc . '</span>';
+	}
+	//Technical info
+	if($tech) {
+		//Leading space is hidden because styles already add visual space
+		$result .= '<span class="hidden"> [</span>';
+		$result .= '<span class="techspec">' . $tech . '</span>';
+		$result .= '<span class="hidden">]</span>';
+	}
+	$result .= '</li>';
+	return $result;
 }
 ?>
