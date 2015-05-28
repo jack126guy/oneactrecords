e9e9d89b9cc35f2b51cda83e903bfa9a3b56d538
Kyanos <someanon126@gmail.com>
2015-05-28 11:38:38 -0700

    Add block element to error message in free template

--- templates/free.php
+++ templates/free.php
@@ -2,7 +2,7 @@
 if(!empty($tparams['releaseid']) && ($tparams['secpos'] >= 0)) {
 	$freesecquery = $sql->query('SELECT seccontent FROM ' . $sql->get_table_prefix() . 'freesecs WHERE releaseid = "' . $tparams['releaseid'] . '" AND secpos = ' . $tparams['secpos']);
 	if($sql->error()) {
-		echo 'Error occured getting the content. Sorry :( Technical info: ' . $sql->error();
+		echo '<p>Error occured getting the content. Sorry :( Technical info: ' . $sql->error() . '</p>';
 	} else {
 		if($sql->num_rows($freesecquery) > 0) {
 			$freesecrow = $sql->fetch_assoc($freesecquery);
