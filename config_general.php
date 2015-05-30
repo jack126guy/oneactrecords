172c04ab9dd6e88dfb42f7ac0c8003849fd0a2bf
Kyanos <someanon126@gmail.com>
2015-05-30 07:48:06 -0700

    Add region to locale configuration

--- config_general.php
+++ config_general.php
@@ -1,5 +1,5 @@
 <?php
 $config['artist'] = 'One Act';
 $config['locale_language'] = 'en';
-$config['locale_region'] = ''; //Leave this blank unless absolutely necessary
+$config['locale_region'] = 'US'; //Needed for Open Graph (Facebook)
 ?>
